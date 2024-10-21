41143afbbf9add69e2b2df539a54af00b085c5fe
facubara <facundo_732@hotmail.com>
2024-10-21 14:59:58 -0300

    video 2 animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public bool isMoving;
     public Vector2 input;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -14,28 +20,39 @@ public class PlayerController : MonoBehaviour
         {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
+
+            Debug.Log("This is input.x " + input.x);
+            Debug.Log("This is input.y" + input.y);
+
             if (input.x != 0) input.y = 0;
+
             if(input != Vector2.zero)
             {
+
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
                 StartCoroutine(Move(targetPos));
             }
         }
+
+        animator.SetBool("isMoving", isMoving);
     }
 
     IEnumerator Move(Vector3 targetPos)
     {
         isMoving = true;
-        //while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        //{
-        //    transform.position = targetPos;
-        //    //transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-        //    yield return null;
-        //}
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            //transform.position = targetPos;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
         transform.position = targetPos;
         isMoving = false;
-        yield return null;
+        //yield return null;
     }
 }
