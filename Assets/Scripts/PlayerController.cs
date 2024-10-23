e7fa4e753098a3e9750abc1e7117323710d2c01e
facubara <facundo_732@hotmail.com>
2024-10-23 19:17:09 -0300

    dialog box and text part 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    private void Update()
+    public void HandleUpdate()
     {
         if (!isMoving)
         {
