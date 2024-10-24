3a258250370ddf7c78381f9c1467dce2fe143bea
facubara <facundo_732@hotmail.com>
2024-10-24 12:02:00 -0300

    part 2 dialog box

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,26 +11,53 @@ public class DialogManager : MonoBehaviour
 
     [SerializeField] int lettersPerSecond;
 
+    public event Action OnShowDialog;
+    public event Action OnHideDialog;
     public static DialogManager Instance { get; private set; }
-
+    Dialog dialog;
+    int currentLine = 0;
+    bool isTyping;
     private void Awake()
     {
         Instance = this;
     }
 
-    public void ShowDialog(Dialog dialog)
+    public IEnumerator ShowDialog(Dialog dialog)
     {
+        yield return new WaitForEndOfFrame();
+        OnShowDialog?.Invoke();
+        this.dialog = dialog;
         dialogBox.SetActive(true);
         StartCoroutine(TypeDialog(dialog.Lines[0]));
     }
 
     public IEnumerator TypeDialog(string line)
     {
+        isTyping = true;
         dialogText.text = "";
         foreach(var letter in line.ToCharArray())
         {
             dialogText.text += letter;
             yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+        isTyping = false;
+    }
+
+    public void HandleUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Z) && !isTyping)
+        {
+            currentLine++;
+            if(currentLine < dialog.Lines.Count) 
+            {
+                StartCoroutine(TypeDialog(dialog.Lines[currentLine]));
+            }
+            else
+            {
+                currentLine = 0;
+                dialogBox.SetActive(false);
+                OnHideDialog?.Invoke();
+            }
         }
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,6 +10,21 @@ public class GameController : MonoBehaviour
 
     GameState state;
 
+    private void Start()
+    {
+        DialogManager.Instance.OnShowDialog += () =>
+        {
+            state = GameState.Dialog;
+        };
+        DialogManager.Instance.OnHideDialog += () =>
+        {
+            if(state == GameState.Dialog)
+            {
+                state = GameState.FreeRoam;
+            }
+        };
+    }
+
     private void Update()
     {
         if (state == GameState.FreeRoam)
@@ -17,7 +32,7 @@ public class GameController : MonoBehaviour
             playerController.HandleUpdate();
         } else if (state == GameState.Dialog)
         {
-
+            DialogManager.Instance.HandleUpdate();
         } else if (state == GameState.Battle)
         {
 
